fb2a47b7c53db05f38236ebcb09be42fa3671b0f
chenleying <leying.tran@gmail.com>
2014-05-01 19:09:23 +1200

    echo.c builds

--- commands/echo.c
+++ commands/echo.c
@@ -4,5 +4,11 @@
  *  Created on: 14/04/2014
  *      Author: Fraser
  */
+#define _CRT_SECURE_NO_WARNINGS
 
+#include <stdio.h>
 
+/*prints the string*/
+void echoprint(char* instring){
+	printf("%s", instring);
+}
