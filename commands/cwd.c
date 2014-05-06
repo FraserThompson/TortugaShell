45704cce78bdd538640e3431e8208cfa19a73d57
Inceptioj <fraseyboy@gmail.com>
2014-05-06 18:08:48 +1200

    Set up the structure for cross-platform

--- commands/cwd.c
+++ commands/cwd.c
@@ -8,14 +8,29 @@
 #include <stdio.h>
 #include <Windows.h>
 
-/*Returns the path to the cwd*/
-int main(int argc, char *argv[]){
-	char cwd[1024] = "";
+
+/* -------WINDOWS------
+* Returns the CWD in windows
+*/
+void *getCWD_win(char *cwd){
 
 	if (!GetCurrentDirectory(1024, cwd)) {
 		printf("Error getting current directory! %s\n", GetLastError());
 	}
+}
 
+/* -------UNIX------
+* Return the CWD in Unix/Linux
+*/
+char *getCWD_unix(){
+}
+
+/* -----CROSS PLATFORM----
+*Returns the path to the cwd
+*/
+int main(int argc, char *argv[]){
+	char cwd[1024] = "";
+	getCWD_win(cwd);
 	printf("%ws\n", cwd);
-	return 0;
+	return EXIT_SUCCESS;
 }
