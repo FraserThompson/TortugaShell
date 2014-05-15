3bdefd3623565f3cd64a4c7c3054c44e48cd0530
Inceptioj <fraseyboy@gmail.com>
2014-05-15 21:35:44 +1200

    Change of structure to help make cross-platform development easier. You may have to re-add your files to visual studio.

---
+++ main/linux/process_mgmt.c
@@ -0,0 +1,32 @@
+/*
+* process_mgmt.c
+*
+*  Created on: 11/05/2014
+*      Author: Fraser
+*  
+*  Contains methods which help with creating processes on Linux.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "parser.h"
+
+
+/* -------LINUX------
+* Returns the path to the system dir.
+* Return: path to the system dir
+*/
+char *get_system_dir(void){
+	return 0;
+}
+
+/* -------LINUX------
+* Creates a process in Unix/Linux.
+* Parameters: Location of process to spawn
+* Return: Error code, 0 if success.
+*/
+int create_process(char *command, char *params) {
+	return 0;
+
+}
