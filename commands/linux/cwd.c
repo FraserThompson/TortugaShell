e95b9fd6330fa6d8b1f37d76e456658cfc73fc6f
Inceptioj <fraseyboy@gmail.com>
2014-05-17 14:28:01 +1200

    Starting to make commands cross platform

--- commands/linux/cwd.c
+++ commands/linux/cwd.c
@@ -4,18 +4,19 @@
 *  Created on: 6/05/2014
 *      Author: Fraser
 *
-*  Returns the path to the current working directory.
+*  Returns the path to the current working directory in Windows.
 */
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
 #include <wchar.h>
+#include "../cwd.h"
 
 /* -------WINDOWS------
 * Returns the CWD in windows
 */
-void *getCWD_win(){
+void *getCWD(){
 	wchar_t cwd[1024];
 	if (!GetCurrentDirectory(1024, cwd)) {
 		printf("CWD: Error getting current directory! %s\n", GetLastError());
@@ -39,6 +40,6 @@ int main(int argc, char *argv[]){
 		i++;
 	}
 
-	printf("%ws\n", getCWD_win());
+	printf("%ws\n", getCWD());
 	return EXIT_SUCCESS;
 }
---
+++ commands/windows/cwd.c
@@ -0,0 +1,45 @@
+/*
+* cwd.c
+*
+*  Created on: 6/05/2014
+*      Author: Fraser
+*
+*  Returns the path to the current working directory in Windows.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <wchar.h>
+#include "../cwd.h"
+
+/* -------WINDOWS------
+* Returns the CWD in windows
+*/
+void *getCWD(){
+	wchar_t cwd[1024];
+	if (!GetCurrentDirectory(1024, cwd)) {
+		printf("CWD: Error getting current directory! %s\n", GetLastError());
+	}
+	return cwd;
+}
+
+/* -----CROSS PLATFORM----
+*Returns the path to the cwd
+*/
+int main(int argc, char *argv[]){
+	int i = 0;
+
+	//Help message, printed by default if no arguments
+	while (argv){
+		printf("%s\n", argv[i]);
+		if ((strcmp(argv[i], "-help") == 0) || (strcmp(argv[i], "-h") == 0)) {
+			printf("CWD: Prints the path of the current working directory. Usage: cwd\n");
+			return EXIT_SUCCESS;
+		}
+		i++;
+	}
+
+	printf("%ws\n", getCWD());
+	return EXIT_SUCCESS;
+}
