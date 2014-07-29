3a4d25939e9f4ea97ada0fec7a30efddda8facd4
JamesB45 <james.buchanan45@gmail.com>
2014-07-29 17:05:42 +1200

    mkdir

---
+++ Turtle/mkdir/mkdir.c
@@ -0,0 +1,25 @@
+/*
+* mkdir.c
+*
+* Created: 29/07/2014
+* Author: James Buchanan
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+/* Create new directory */
+
+int wmain(int argc, wchar_t *argv[]){
+
+	wchar_t *path = argv[1];
+	if (!CreateDirectory(path, NULL)) {
+		printf("\nCouldn't create %S directory.\n", path);
+	}
+	else {
+		printf("\n%S directory successfully created.\n", path);
+	}
+}
