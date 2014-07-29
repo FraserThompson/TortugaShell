a8a5e385751a5587460d07801dfeb829920b8570
JamesB45 <james.buchanan45@gmail.com>
2014-07-29 15:41:42 +1200

    ls.c

---
+++ SomethingUseful/ls/ls.c
@@ -0,0 +1,42 @@
+/*
+* ls.c
+*
+* Created: 29/07/2014
+* Author: James Buchanan
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+/* Prints out all files in active directory */
+
+int wmain(int argc, wchar_t *argv[]){
+
+	wchar_t sPath[2048];
+	WIN32_FIND_DATA fdFile;
+	HANDLE hFind = NULL;
+	wchar_t *sDir = L".\\*.*";
+
+	//wsprintf(sPath, L"%s\\%s", sDir, fdFile.cFileName);
+	if ((hFind = FindFirstFile(sDir, &fdFile)) == INVALID_HANDLE_VALUE)
+	{
+		wprintf(L"Path not found: [%s]\n", sDir);
+		return NULL;
+	}
+	do {
+		{
+			if (fdFile.dwFileAttributes &FILE_ATTRIBUTE_DIRECTORY)
+			{
+				continue;
+			}
+			else {
+				wprintf(L"%s\n", fdFile.cFileName);
+			}
+		}
+	} while (FindNextFile(hFind, &fdFile));
+	//system("pause");
+}
+	
