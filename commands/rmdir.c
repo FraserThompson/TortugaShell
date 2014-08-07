aabd71cca5c38355d6e0b2d1be1b88284266cbb4
Inceptioj <fraseyboy@gmail.com>
2014-08-07 16:44:20 +1200

    Fixed rmdir so it prints help message and also does errors properly

--- commands/rmdir.c
+++ commands/rmdir.c
@@ -12,14 +12,22 @@
 #include <Windows.h>
 
 /* Remove a directory */
-
 int wmain(int argc, wchar_t *argv[]){
+	int i = 0;
+	//Help message, printed by default if no arguments
+	while (argv[i]){
+		if (argc == 1 || ((wcscmp(argv[i], L"-help") == 0) || (wcscmp(argv[i], L"-h") == 0))) {
+			wprintf(L"rmdir\tPermanently deletes a directory.\n\tUsage: rm [dir]\n");
+			return EXIT_SUCCESS;
+		}
+		i++;
+	}
 
 	wchar_t *path = argv[1];
-	if (RemoveDirectory(path) != 0) {
-		printf("\nCouldn't remove %S directory.\n", path);
+	if (RemoveDirectory(path) == 0) {
+		wprintf(L"\nCouldn't remove %S directory.\n", path);
 	}
 	else {
-		printf("\n%S directory removed successfully.\n", path);
+		wprintf(L"\n%S directory removed successfully.\n", path);
 	}
 }
