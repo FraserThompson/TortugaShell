07a5e484e6da630df08701e1879997546f20bcd3
JamesB45 <james.buchanan45@gmail.com>
2014-08-10 14:57:46 +1200

    mkdir help

--- commands/mkdir.c
+++ commands/mkdir.c
@@ -12,14 +12,21 @@
 #include <Windows.h>
 
 /* Create new directory */
-
 int wmain(int argc, wchar_t *argv[]){
-
-	wchar_t *path = argv[1];
-	if (!CreateDirectory(path, NULL)) {
-		wprintf(L"\nCouldn't create %ls directory.\n", path);
+	int i = 0;
+	//Help message, printed by default if no arguments
+	while (argv[i]){
+		if (argc == 1 || ((wcscmp(argv[i], L"-help") == 0) || (wcscmp(argv[i], L"-h") == 0))) {
+			wprintf(L"rmdir\tPermanently deletes a directory.\n\tUsage: rm [dir]\n");
+			return EXIT_SUCCESS;
+		}
+		i++;
+	}
+	
+	if (!CreateDirectory(argv[1], NULL)) {
+		wprintf(L"\nCouldn't create %ls directory.\n", argv[1]);
 	}
 	else {
-		wprintf(L"\n%ls directory successfully created.\n", path);
+		wprintf(L"\n%ls directory successfully created.\n", argv[1]);
 	}
 }
